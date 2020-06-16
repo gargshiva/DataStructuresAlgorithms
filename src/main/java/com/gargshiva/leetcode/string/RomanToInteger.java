bbc02ed1a39fc97a53a834c16d821d34251b0aba
shiva garg <shivagarg91@gmail.com>
2020-06-17 00:40:56 +0100

    Romans to Decimal

---
+++ src/main/java/com/gargshiva/leetcode/string/RomanToInteger.java
@@ -0,0 +1,89 @@
+package com.gargshiva.leetcode.string;
+
+/**
+ * https://leetcode.com/problems/roman-to-integer/
+ *
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ *
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ *
+ *
+ * Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+ *
+ *
+ */
+public class RomanToInteger {
+
+    public static void main(String[] args) {
+        int op = romanToInt("MCMXCIV");
+        System.out.println(op);
+    }
+
+    public static int romanToInt(String s) {
+
+        int answer = 0;
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = 1;
+        while (j < arr.length) {
+            if (value(arr[i]) >=value(arr[j])) {
+                answer = answer + value(arr[i]);
+                ++i;
+                ++j;
+            } else {
+                answer = answer + (value(arr[j]) - value(arr[i]));
+                i = i + 2;
+                j = j + 2;
+            }
+        }
+
+        if (i < arr.length) {
+            answer = answer + value(arr[i]);
+        }
+        return answer;
+    }
+
+
+    public static int value(char c) {
+        int value = 0;
+        switch (c) {
+            case 'I':
+                value = 1;
+                break;
+            case 'V':
+                value = 5;
+                break;
+            case 'X':
+                value = 10;
+                break;
+            case 'L':
+                value = 50;
+                break;
+            case 'C':
+                value = 100;
+                break;
+            case 'D':
+                value = 500;
+                break;
+            case 'M':
+                value = 1000;
+                break;
+
+        }
+        return value;
+    }
+}
